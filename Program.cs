8ed27f473dac2bd8339590d95e76ab011f8ecdfd
NJ <jainnitin2411@gmail.com>
2024-08-14 18:29:17 +0530

    Created new project

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using System;
 
-namespace HelloWeb
+namespace dotnet_core_hello_world
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-
-            var config = new ConfigurationBuilder()
-                          .AddCommandLine(args)
-                          .Build();
-            var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
+            Console.WriteLine("Hello World!");
         }
     }
 }
