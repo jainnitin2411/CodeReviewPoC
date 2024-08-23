c723af388cf65891092e0fb104595fffd1f92b32
Anirban Ghosh <anirban.ghosh@corecard.com>
2024-08-23 20:14:40 +0530

    testtt

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace dotnet_core_hello_world
 
             // Vulnerable SQL Query
 
-            string connectionString = "your_connection_string_here Anirban";
+            string connectionString = "your_connection_string_here No Ani";
 
             string query = $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
 
