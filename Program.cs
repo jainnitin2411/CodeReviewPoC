c25c12df187d05920cdf66dab53451c64b163069
NJ <jainnitin2411@gmail.com>
2024-08-23 19:55:39 +0530

    Changes

--- Program.cs
+++ Program.cs
@@ -1,26 +1,78 @@
 ﻿using System;
+
 using System.Threading.Tasks;
 
-namespace dotnet_core_hello_world
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            LogMsg();
-            string username = "admin"; // Hardcoded username
-            string password = "password123"; // Hardcoded password
+using System.Data.SqlClient;
 
-            if (username == "admin" && password == "password123")
+namespace dotnet_core_hello_world
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter your username:");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+
+            string password = Console.ReadLine();
+
+            // Vulnerable SQL Query
+
+            string connectionString = "your_connection_string_here";
+
+            string query = $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
             {
-                Console.WriteLine("Welcome Admin!");
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+
+                {
+
+                    Console.WriteLine("Login successful!");
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("Invalid username or password.");
+
+                }
+
+                reader.Close();
+
             }
+
+            LogMsg();
 
         }
 
         private async static Task LogMsg()
+
         {
+
             Console.WriteLine("Hello Nitin Jain. How are you? !!!!!!Anirban!!!!");
+
         }
+
     }
+
 }
+
